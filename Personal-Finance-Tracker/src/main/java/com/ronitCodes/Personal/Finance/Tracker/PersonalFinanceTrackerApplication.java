0704ba919c9688ff37352777d530ada899886f5e
ronneet <ronitstha17@gmail.com>
2025-05-24 18:43:11 +0545

    First Commit

---
+++ Personal-Finance-Tracker/src/main/java/com/ronitCodes/Personal/Finance/Tracker/PersonalFinanceTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.ronitCodes.Personal.Finance.Tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalFinanceTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalFinanceTrackerApplication.class, args);
+	}
+
+}
